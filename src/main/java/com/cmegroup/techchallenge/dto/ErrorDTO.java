b4ee9a598618a18a5fa4c7892b7f417e5fabf416
Bigger, Thomas <thomasbigger584@gmail.com>
2023-06-23 16:21:40 +0100

    Adding more tests

--- src/main/java/com/cmegroup/techchallenge/dto/ErrorDTO.java
+++ src/main/java/com/cmegroup/techchallenge/dto/ErrorDTO.java
@@ -3,6 +3,10 @@ package com.cmegroup.techchallenge.dto;
 public class ErrorDTO {
     private final String message;
 
+    public ErrorDTO() {
+        this.message = null;
+    }
+
     public ErrorDTO(String message) {
         this.message = message;
     }
--- src/test/java/com/cmegroup/techchallenge/rest/PalindromeResourceIntegrationTest.java
+++ src/test/java/com/cmegroup/techchallenge/rest/PalindromeResourceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.cmegroup.techchallenge.rest;
 
 
 import com.cmegroup.techchallenge.TechChallengeApplication;
+import com.cmegroup.techchallenge.dto.ErrorDTO;
 import com.cmegroup.techchallenge.dto.PalindromeDTO;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,6 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -16,7 +18,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = TechChallengeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        classes = TechChallengeApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PalindromeResourceIntegrationTest {
     private static final TestRestTemplate REST_TEMPLATE = new TestRestTemplate();
 
@@ -25,12 +29,12 @@ class PalindromeResourceIntegrationTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"madam", "kayak", ""})
-    public void testGetPalindrome_IsSuccessful(String value) {
+    public void testGetPalindrome_IsSuccessful_ReturnsTrue(String value) {
         String url = createURLWithPort("/palindrome?value=" + value);
 
         ResponseEntity<PalindromeDTO> response =
                 REST_TEMPLATE.getForEntity(url, PalindromeDTO.class);
-        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        assertEquals(HttpStatusCode.valueOf(HttpStatus.OK.value()), response.getStatusCode());
 
         PalindromeDTO palindrome = response.getBody();
         assertNotNull(palindrome);
@@ -38,9 +42,50 @@ class PalindromeResourceIntegrationTest {
         assertTrue(palindrome.isPalindrome());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"palindrome", "technical", "challenge"})
+    public void testGetPalindrome_IsSuccessful_ReturnsFalse(String value) {
+        String url = createURLWithPort("/palindrome?value=" + value);
+
+        ResponseEntity<PalindromeDTO> response =
+                REST_TEMPLATE.getForEntity(url, PalindromeDTO.class);
+        assertEquals(HttpStatusCode.valueOf(HttpStatus.OK.value()), response.getStatusCode());
+
+        PalindromeDTO palindrome = response.getBody();
+        assertNotNull(palindrome);
+        assertEquals(value, palindrome.getValue());
+        assertFalse(palindrome.isPalindrome());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"madam1", "2kayak", "123", "ma1d2am"})
+    public void testGetPalindrome_IsFailure_NoNumbersAllowed(String value) {
+        String url = createURLWithPort("/palindrome?value=" + value);
+
+        ResponseEntity<ErrorDTO> response =
+                REST_TEMPLATE.getForEntity(url, ErrorDTO.class);
+        assertEquals(HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()), response.getStatusCode());
+
+        ErrorDTO error = response.getBody();
+        assertNotNull(error);
+        assertTrue(error.getMessage().contains("Palindrome value should not contain a number:"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"madam ", " kayak", "   ", "ma d am"})
+    public void testGetPalindrome_IsFailure_NoSpacesAllowed(String value) {
+        String url = createURLWithPort("/palindrome?value=" + value);
+
+        ResponseEntity<ErrorDTO> response =
+                REST_TEMPLATE.getForEntity(url, ErrorDTO.class);
+        assertEquals(HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()), response.getStatusCode());
+
+        ErrorDTO error = response.getBody();
+        assertNotNull(error);
+        assertTrue(error.getMessage().contains("Palindrome value should not contain any spaces"));
+    }
+
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
     }
-
-
 }
